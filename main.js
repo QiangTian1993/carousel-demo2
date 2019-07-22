06b4e5f62465b9cd861c32885f396419a7b89858
Time <244439359@qq.com>
2019-07-22 15:02:40 +0800

    add event.resize() and 优化代码

--- main.js
+++ main.js
@@ -8,56 +8,35 @@ for (let i = 0; i < imgUrl.length; i++) {
 }
 
 let pageWdith = $(window).width()
+initialWidth()
 changeWidth()
 makeFakeSlides()
-
-$('.window .imgs').css({
-    transform: `translateX(-${1 * pageWdith}px)`,
-
-})
+initiaTransform(0)
 
 $('nav ul').on('click', 'li', (e) => {
     let index = $(e.currentTarget).index()
     if (n === imgUrl.length - 1 && index === 0) {
-        $('.window .imgs').css({
-            transform: `translateX(-${(imgUrl.length + 1) * pageWdith}px)`,
-        })
-            .one('transitionend', () => {
-                $('.window .imgs').hide()
-                    .offset()
-                $('.window .imgs').css({
-                    transform: `translateX(-${pageWdith}px)`,
-                })
-                    .show()
-            })
-        $('section h1').text(titles[0].h1)
-        $('section h2').text(titles[0].h2)
+        changeTransform(imgUrl.length + 1, index)
+        changeTitle(titles[0])
     } else if (n === 0 && index === imgUrl.length - 1) {
-        $('.window .imgs').css({
-            transform: `translateX(0px)`,
-        })
-            .one('transitionend', () => {
-                $('.window .imgs').hide()
-                    .offset()
-                $('.window .imgs').css({
-                    transform: `translateX(-${(index + 1) * pageWdith}px)`,
-                })
-                    .show()
-            })
-        $('section h1').text(titles[imgUrl.length - 1].h1)
-        $('section h2').text(titles[imgUrl.length - 1].h2)
-
+        changeTransform(0, index)
+        changeTitle(titles[imgUrl.length - 1])
     } else {
-        $('.window .imgs').css({
-            transform: `translateX(-${(index + 1) * pageWdith}px)`,
-        })
-        $('section h1').text(titles[index].h1)
-        $('section h2').text(titles[index].h2)
+        initiaTransform(index)
+        changeTitle(titles[index])
+        changeWidth(index)
     }
     n = index
+
 })
 
-function changeWidth() {
+function initiaTransform(index) {
+    $('.window .imgs').css({
+        transform: `translateX(-${(index + 1) * pageWdith}px)`,
+    })
+}
+
+function initialWidth() {
     $('.window').css({
         width: pageWdith + 'px'
     })
@@ -67,9 +46,48 @@ function changeWidth() {
     })
 }
 
+function changeWidth(index) {
+    $(window).resize(() => {
+        if (n === 0) {
+            pageWdith = $(window).width()
+            initialWidth()
+            $('.window .imgs').css({
+                transform: `translateX(-${pageWdith}px)`,
+            })
+        } else {
+
+            pageWdith = $(window).width()
+            initialWidth()
+            $('.window .imgs').css({
+                transform: `translateX(-${(index + 1) * pageWdith}px)`,
+            })
+
+        }
+    })
+}
+
 function makeFakeSlides() {
     let $lastImg = $('.window .imgs a').eq(0).clone(true)
     let $firstImg = $('.window .imgs a').eq(imgUrl.length - 1).clone(true)
     $('.window .imgs').append($lastImg)
     $('.window .imgs').prepend($firstImg)
+}
+
+function changeTitle(titlesLength) {
+    $('section h1').text(titlesLength.h1)
+    $('section h2').text(titlesLength.h2)
+}
+
+function changeTransform(moveNumber, index) {
+    $('.window .imgs').css({
+        transform: `translateX(-${(moveNumber) * pageWdith}px)`,
+    })
+        .one('transitionend', () => {
+            $('.window .imgs').hide()
+                .offset()
+            $('.window .imgs').css({
+                transform: `translateX(-${(index + 1) * pageWdith}px)`,
+            })
+                .show()
+        })
 }
